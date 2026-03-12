81835b4270d72b494e2413e66616f25d7581650a
Lessa Apolinario <lessaapolinario@gmail.com>
2026-03-12 20:58:06 -0300

    Finish auth service token manipulation

--- src/infra/auth/auth.service.ts
+++ src/infra/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserRequest } from 'src/core/@types/http/request/CreateUserRequest';
 import { CredentialsRequest } from 'src/core/@types/http/request/CredentialsRequest';
@@ -15,15 +15,45 @@ export class AuthService extends AuthUseCase {
     super();
   }
 
-  login(credentials: CredentialsRequest): Promise<AuthResponse> {
-    throw new Error('Method not implemented.');
+  private createAuthTokens(payload: { sub: string }) {
+    const accessToken = this.jwtService.sign(payload);
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d' });
+
+    return { accessToken, refreshToken };
   }
 
-  register(user: CreateUserRequest): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  async login(credentials: CredentialsRequest): Promise<AuthResponse> {
+    const foundUser = await this.authAdapter.login(credentials);
+
+    const payload = {
+      sub: foundUser.id,
+    };
+
+    return this.createAuthTokens(payload);
   }
 
-  refresh(token: string): Promise<AuthResponse> {
-    throw new Error('Method not implemented.');
+  async register(user: CreateUserRequest): Promise<boolean> {
+    return await this.authAdapter.register(user);
+  }
+
+  async refresh(token: string): Promise<AuthResponse> {
+    const isTokenValid = this.jwtService.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    if (!isTokenValid) {
+      throw new UnauthorizedException('Invalid token.');
+    }
+
+    const payload = this.jwtService.decode(token);
+
+    const currentTimeInMilliseconds = Date.now() / 1000;
+    const isTokenExpired = payload.exp < currentTimeInMilliseconds;
+
+    if (isTokenExpired) {
+      throw new UnauthorizedException('Token expired.');
+    }
+
+    return this.createAuthTokens(payload);
   }
 }
